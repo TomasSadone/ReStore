6a5951934f2883e7b27399e9ff917020d0245389
TomasSadone <tomisadone27@gmail.com>
2023-07-08 19:21:51 -0300

    configuraciones login terceros

--- Frontend/restore/next.config.js
+++ Frontend/restore/next.config.js
@@ -1,45 +1,46 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env:{
-    GOOGLE_CLIENT_ID:"632939467428-km8glfmnqemr9sl63902dd84lfb98fne.apps.googleusercontent.com",
-    GOOGLE_CLIENT_SECRET:"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",
-    NEXTAUTH_URL:"http://localhost:3000",
-    NEXTAUTH_URL_INTERNAL:"http://localhost:3000",
-    NEXTAUTH_SECRET:"dPmTCswUFo6w6Xr0XufjUCq0jU8QuZhBZRktZTVruj8=",
-    GITHUB_ID:"57b09d40c3ea5aaedfc1",
-    GITHUB_SECRET:"dbc1c06436c306c3aba2f9703344c8432d8865c3"
+  env: {
+    GOOGLE_CLIENT_ID:
+      '526949457450-l4ik6plsg5up6ar09co4lcda0l3j36ff.apps.googleusercontent.com',
+    GOOGLE_CLIENT_SECRET: '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',
+    NEXTAUTH_URL: 'http://localhost:3000',
+    NEXTAUTH_URL_INTERNAL: 'http://localhost:3000',
+    NEXTAUTH_SECRET: 'dPmTCswUFo6w6Xr0XufjUCq0jU8QuZhBZRktZTVruj8=',
+    GITHUB_ID: '9be415a33af95f31745b',
+    GITHUB_SECRET: 'e6348e9089477b34763431b3a814089f77661071',
   },
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "imagekit.androidphoria.com",
-        port: "",
+        protocol: 'https',
+        hostname: 'imagekit.androidphoria.com',
+        port: '',
       },
       {
-        protocol: "https",
-        hostname: "i.ytimg.com",
-        port: "",
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+        port: '',
       },
       {
-        protocol: "https",
-        hostname: "s3.studytonight.com",
-        port: "",
+        protocol: 'https',
+        hostname: 's3.studytonight.com',
+        port: '',
       },
       {
-        protocol: "https",
-        hostname: "http2.mlstatic.com",
-        port: "",
+        protocol: 'https',
+        hostname: 'http2.mlstatic.com',
+        port: '',
       },
       {
-        protocol: "https",
-        hostname: "images.pexels.com",
-        port: "",
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        port: '',
       },
       {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        port: "",
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
       },
       {
         protocol: 'https',
@@ -72,5 +73,3 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
-
-
